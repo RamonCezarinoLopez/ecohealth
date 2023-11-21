67f4e67f9c7c1c88e8499e3a78957a797f454909
RamonCezarinoLopez <rm551279@fiap.com.br>
2023-11-21 20:17:41 -0300

    subindo galeria de cards

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -2,7 +2,7 @@ import './Card.css'
 export default function Card(){
     return(
         <div className="card">
-      <img src="/saude.jpg" alt="Imagem da Notícia" className="card-image" />
+      <img src="https://i1.wp.com/petsbagunceiros.com/wp-content/uploads/2020/03/cachorro-racao.png?fit=1024%2C682&ssl=1" alt="Imagem da Notícia" className="card-image" />
       <div className="card-content">
         <h2>Título da Notícia</h2>
         <p>Descrição da notícia. Clique abaixo para mais detalhes.</p>
---
+++ src/app/components/galeria/Galeria.jsx
@@ -0,0 +1,17 @@
+import Card from '../card/Card'
+import './Galeria.css'
+
+const Galeria = () => {
+  return (
+    <div className='galeria'>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+    </div>
+  )
+}
+
+export default Galeria
